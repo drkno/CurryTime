1bb1bc5945b7265664d49efff7b54a0dca76feaf
Matthew Knox <matthew.knox@orionhealth.com>
2015-12-02 14:46:33 +1300

    Persistent Curry

--- core/persistence.js
+++ core/persistence.js
@@ -1,8 +1,21 @@
-//var saveLocation =
+var path = require('path'),
+	home = process.env.HOME || process.env.USERPROFILE,
+	configPath = path.join(home, '.curryconfig'),
+	config = null,
+	fs = require('fs');
+
+try {
+	config = require(configPath);
+}
+catch (e) {
+	config = {};
+}
 
 exports.getKey = function(key) {
-	return null;
+	return config[key];
 };
 
 exports.updateKey = function(key, value) {
+	config[key] = value;
+	fs.writeFileSync(configPath, JSON.stringify(config, null, 4), 'utf8');
 };
